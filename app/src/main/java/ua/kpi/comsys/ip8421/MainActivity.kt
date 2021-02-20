2de851bc93dfc0e7b3de20bcf0f8c6de465b1c40
DjBee0312 <pashapasha0312@gmail.com>
2021-02-20 18:51:21 +0200

    lab5

--- app/src/main/java/ua/kpi/comsys/ip8421/MainActivity.kt
+++ app/src/main/java/ua/kpi/comsys/ip8421/MainActivity.kt
@@ -1,13 +1,14 @@
 package ua.kpi.comsys.ip8421
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,13 @@ class MainActivity : AppCompatActivity() {
         var viewPager2 : ViewPager2 = findViewById(R.id.id_viewPager2);
 
         viewPager2.adapter = object : FragmentStateAdapter(this) {
-            override fun getItemCount(): Int = 3
+            override fun getItemCount(): Int = 4
             override fun createFragment(position: Int): Fragment {
                 return when(position){
                     0 -> Page1();
                     1 -> Page2();
                     2 -> BookActivity();
+                    3 -> Page4();
                     else -> error("Error");
                 }
             }
@@ -32,19 +34,24 @@ class MainActivity : AppCompatActivity() {
 
         tabLayout.getTabAt(0)?.apply {
             customView?.setOnClickListener(){
-                viewPager2.setCurrentItem(0)
+                viewPager2.currentItem = 0
             }
         }
 
         tabLayout.getTabAt(1)?.apply {
             customView?.setOnClickListener(){
-                viewPager2.setCurrentItem(1)
+                viewPager2.currentItem = 1
             }
         }
 
         tabLayout.getTabAt(2)?.apply {
             customView?.setOnClickListener(){
-                viewPager2.setCurrentItem(2)
+                viewPager2.currentItem = 2
+            }
+        }
+        tabLayout.getTabAt(3)?.apply {
+            customView?.setOnClickListener(){
+                viewPager2.currentItem = 3
             }
         }
         TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
@@ -57,5 +64,14 @@ class MainActivity : AppCompatActivity() {
         tabLayout.getTabAt(1)?.text = "Other";
         tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_books);
         tabLayout.getTabAt(2)?.text = "Books";
+        tabLayout.getTabAt(3)?.setIcon(R.drawable.ic_pictures);
+        tabLayout.getTabAt(3)?.text = "Pictures";
+
+
+
     }
+
+
+
 }
+
